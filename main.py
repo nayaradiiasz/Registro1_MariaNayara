dc942dc8fb8793fe54ae1c17240d7f145669f110
ProfaAudy <167873774+ProfaAudy@users.noreply.github.com>
2024-08-16 14:33:34 -0300

    Update main.py
    Correção de bugs e atualização de feature multiplicação até 10

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ for i in range(1,10):
   print("Loop")
 #Funcionalidade peidr valor ao usuário até ele digitar 0
 user = int(input("Insira um número: "))
-while user == 0:
+while user != 0:
   print("Diferente de Zero")
   user = int(input("Insira outro número: "))
 print("Saiu do loop")
 
 #Funcionalidade multiplicação até 10
+user = int(input("Insira um número: "))
+for i in range(0, user):
+  print(f"{user} x {i} = {user*i}")
