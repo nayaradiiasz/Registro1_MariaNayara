7a620ec15a0f4acee36fdc383e5584c4ee1b7546
ProfaAudy <167873774+ProfaAudy@users.noreply.github.com>
2024-08-16 14:28:47 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#Programa principal do projeto XYZ
+print("Bem vindo ao projeto XYZ")
+#Funcionalidade de Loop até 10
+for i in range(1,10):
+  print("Loop")
+#Funcionalidade peidr valor ao usuário até ele digitar 0
+user = int(input("Insira um número: "))
+while user == 0:
+  print("Diferente de Zero")
+  user = int(input("Insira outro número: "))
+print("Saiu do loop")
+
+#Funcionalidade multiplicação até 10
